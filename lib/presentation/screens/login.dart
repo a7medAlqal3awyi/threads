e41a2227499be1bc4d794777581b9a0e2cc93464
Ahmed <a7medalqalawyi@bmail.com>
2023-08-10 14:09:53 +0300

    some refactors in login screen

--- lib/presentation/screens/login.dart
+++ lib/presentation/screens/login.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:threads/core/extensions.dart';
+import 'package:threads/presentation/screens/bottom_bar/bottom_bar.dart';
+import 'package:threads/presentation/screens/home/home.dart';
+import 'package:threads/presentation/widgets/custom_text_button.dart';
 import 'package:threads/presentation/widgets/text_form_of_auth.dart';
+
+import '../widgets/custom_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -12,58 +17,37 @@ class LoginScreen extends StatelessWidget {
       backgroundColor: const Color(0xFF0F212F),
       body: SingleChildScrollView(
         child: Padding(
-          padding:  EdgeInsets.symmetric(horizontal: context.deviceWidth/15),
+          padding: EdgeInsets.symmetric(horizontal: context.deviceWidth / 15),
           child: SizedBox(
             height: context.deviceHeight,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Gap(context.deviceHeight / 8),
+                Gap(context.deviceHeight / 7),
                 const Text(
                   "English(US)",
                   style: TextStyle(color: Colors.grey, fontSize: 16),
                 ),
                 const Spacer(),
                 Image.asset("assets/images/insta_logo.png"),
-                Gap(context.deviceHeight /30),
-
+                Gap(context.deviceHeight / 30),
                 const AuthTextFormField(
                   labelText: 'User name,email or mobile num...',
                 ),
                 Gap(context.deviceHeight / 90),
-
                 const AuthTextFormField(
                   labelText: 'Password',
                 ),
                 Gap(context.deviceHeight / 90),
-
-                SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        Colors.blue,
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      "Log in",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                Gap(context.deviceHeight / 300),
+                CustomButton(
+                  label: "Log in",
+                  onPressed: () {
+                    context.push(const BottomBar());
+                  },
                 ),
-                Gap(context.deviceHeight /300),
-
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Forget password?",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
+                 CustomTextButton(text: "Forgot Password?",onPressed: (){},),
                 const Spacer(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
